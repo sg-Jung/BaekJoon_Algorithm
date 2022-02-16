a54b02d4a1df0b2c8237a0d71680a655da611a63
SangGyeong <alow9942@daum.net>
2022-02-16 14:47:05 +0900

    .

--- BaekJun/11399.cpp
+++ BaekJun/11399.cpp
@@ -8,5 +8,31 @@ using namespace std;
 
 int main()
 {
+	int N, temp;
+	int sum = 0;
+	cin >> N;
 
+	vector<int> time = vector<int>();
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> temp;
+		time.push_back(temp);
+	}
+
+	sort(time.begin(), time.end());
+
+	temp = 0;
+
+	for (int i = 0; i < time.size(); i++)
+	{
+		for (int j = 0; j < i + 1; j++)
+		{
+			temp += time[j];
+		}
+
+		sum += temp;
+		temp = 0;
+	}
+	cout << sum;
 }
